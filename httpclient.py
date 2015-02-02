1bb569bca1d005dbd99b90916dce5543905b499d
curopium <curnow@ualberta.ca>
2015-02-01 18:59:07 -0700

    implimented get and post methods

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 Abram Hindle
+# Copyright 2015 Thomas Curnow
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,6 +23,8 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
+# used for getting the host from a url
+from urlparse import urlparse
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
@@ -36,17 +38,30 @@ class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
-        # use sockets!
-        return None
+        #if there is no port, make it 80
+        if(port == None):
+            port = 80
+        #tries to create socket
+        try:
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error as msg:
+            print("failed to create socket")
+            print('Error code: ' + str(msg[0]) + ' , Error message: ' + msg[1])
+            sys.exit()
+
+        s.connect((host, port))
+        return s
 
     def get_code(self, data):
-        return None
+        header = self.get_headers(data)
+        code = header.split(' ')[1]
+        return code
 
     def get_headers(self,data):
-        return None
+        return data.split('\r\n\r\n')[0]
 
     def get_body(self, data):
-        return None
+        return data.split('\r\n\r\n')[1]
 
     # read everything from the socket
     def recvall(self, sock):
@@ -63,13 +78,69 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        url = urlparse(url)
+        host = url.hostname
+        directory = url.path
+    
+        if(directory == ''):
+            directory = '/'
+        
+        header = 'GET ' + directory + ' HTTP/1.1\r\n'
+        header = header + 'Host: ' + host + '\r\n'
+        header = header + 'Accept: */*\r\n'
+        header = header + 'Connection: close\r\n'
+        header = header + '\r\n'
+         
+        sock = self.connect(host, url.port)
+        sock.send(header)
+        sock_return = self.recvall(sock)
+
+        code = int(self.get_code(sock_return))
+        body = self.get_body(sock_return)
+
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+
+        url = urlparse(url)
+        host = url.hostname
+        directory = url.path
+        if(directory == ''):
+            directory = '/'
+        
+        if(args == None):
+            content_length = 0;
+        else:
+            send_body = urllib.urlencode(args)
+            content_length = len(send_body)            
+
+        #Create the header to be sent to socket
+        header = 'POST '+directory+' HTTP/1.1\r\n'
+        header = header + 'Host: ' + host +'\r\n'
+        header = header + 'Accept: */*\r\n'
+        header = header + 'Content-Length: ' + str(content_length) + '\r\n'
+        header = header + 'Content-Type:application/x-www-form-urlencoded \r\n'
+        header = header + 'Connection: close\r\n'
+        header = header + '\r\n'
+
+        #add the body only if there is a body
+        if(content_length > 0):
+            header = header + send_body
+            header = header + '\r\n\r\n'
+
+        sock = self.connect(host, url.port)
+        sock.send(header)
+        sock_return = self.recvall(sock)
+
+        code = int(self.get_code(sock_return))
+        body = self.get_body(sock_return)
+        
         return HTTPRequest(code, body)
 
+    #def command(self, url, command="GET", args=None):
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
             return self.POST( url, args )
@@ -79,6 +150,7 @@ class HTTPClient(object):
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
+
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
@@ -86,3 +158,4 @@ if __name__ == "__main__":
         print client.command( sys.argv[1], sys.argv[2] )
     else:
         print client.command( command, sys.argv[1] )    
+        
